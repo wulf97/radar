a2b30afc6d8c20181e39c2b6e27f19df67f69b50
Alexej <alexej4703@gmail.com>
2021-04-16 07:17:57 +0300

    Add lockfile

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QFile>
+#include <QLockFile>
+#include <QDir>
+#include <QMessageBox>
+
 
 #include "MainWindow.h"
 
@@ -11,14 +15,26 @@ int main(int argc, char **argv) {
 
     std::srand(std::time(nullptr));
 
-    MainWindow mainWindow;
-
+    // Загрузка стилей
     QFile styleFile(":/res/stylesheet/style.qss");
     styleFile.open(QFile::ReadOnly);
     QString style(styleFile.readAll());
     styleFile.close();
 
     app.setStyleSheet(style);
+
+    // Проверка на предмет запуска нескольких программ
+    QLockFile lockFile(QDir::temp().absoluteFilePath("<uniq id>.lock"));
+
+    if(!lockFile.tryLock(100)){
+        QMessageBox msgBox;
+        msgBox.setIcon(QMessageBox::Warning);
+        msgBox.setText(QObject::tr("Программа уже запущена"));
+        msgBox.exec();
+        return 1;
+    }
+
+    MainWindow mainWindow;
 
     mainWindow.move(QApplication::desktop()->screen()->rect().center()
                     - mainWindow.frameGeometry().center());
